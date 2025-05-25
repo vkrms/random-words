7ed58498d739e50a13896c4083559b6705ff25c4
Pavel M <april504@yandex.ru>
2025-05-25 18:42:20 +0700

    fix: tailwind styles

--- src/components/word.tsx
+++ src/components/word.tsx
@@ -6,7 +6,7 @@ function Word({ array }: { array: string[] }) {
     const word = array[Math.floor(Math.random() * array.length)]
 
     return (
-        <span className="text-xl font-bold text-foreground px-3 py-1 bg-muted rounded-md">
+        <span className="text-xl font-bold text-foreground px-3 py-1 bg-muted rounded-md bg-gray-200">
             {word}
         </span>
     )
